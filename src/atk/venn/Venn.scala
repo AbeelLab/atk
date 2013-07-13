fdabe4c5de85165fb3a3d916e25f3e047046cea2
Thomas Abeel <thomas@abeel.be>
2013-07-13 15:13:15 -0400

    venn diagrams

---
+++ src/atk/venn/Venn.scala
@@ -0,0 +1,69 @@
+package venn
+
+import scala.io.Source
+import org.apache.batik.transcoder.image.PNGTranscoder
+import org.apache.batik.transcoder.TranscoderInput
+import java.io.ByteArrayInputStream
+import org.apache.batik.transcoder.SVGAbstractTranscoder
+import java.io.FileOutputStream
+import org.apache.batik.transcoder.TranscoderOutput
+import java.io.File
+/**
+ * Helper object to make Venn diagrams
+ *
+ */
+object Venn {
+
+  /**
+   * List is a list of pairs where the first piece is the label of the set and the second part is all the elements belonging to that set.
+   *
+   */
+  def makeVenn(outFile: File, list: List[(String, List[String])]) {
+
+    val membership = scala.collection.mutable.Map.empty[String, String]
+    val chars = ('A' to 'E').toList //List('A', 'B', 'C', 'D', 'E')
+    assume(list.length <= chars.length && list.length >= 2)
+
+    val labels = list.map(_._1)
+
+    val labelMap = chars.zip(labels).toMap
+
+    for ((label, l) <- list) {
+      val letter = chars(labels.indexOf(label))
+      l.map(e => {
+        membership.update(e, membership.getOrElse(e, "") + letter)
+      })
+
+    }
+
+    val counts = membership.values.groupBy(x => x).mapValues(_.size)
+
+    var venn = Source.fromFile("resources/venn/venn" + list.length + "a.svg").mkString
+
+    val keyCombinations = (1 to chars.size).flatMap(chars.combinations).toList
+
+    for (key <- keyCombinations.map(_.mkString("")))
+      venn = venn.replaceAll(">" + key + "<", ">" + counts.getOrElse(key, 0) + "<");
+
+    for ((key, label) <- labelMap)
+      venn = venn.replaceAll(">List" + key + "<", ">" + label + "<");
+
+    /* Convert to PNG */
+    val streamBytes = venn.getBytes()
+    val t = new PNGTranscoder();
+    val input = new TranscoderInput(new ByteArrayInputStream(streamBytes));
+
+    t.addTranscodingHint(SVGAbstractTranscoder.KEY_WIDTH, 800.0f);
+    // GraphicsFileExport.exportPNG(new ImageDrawable(img), CompareConfig.folder + "/venn." + genome + "." + infix + ".png", 800, 600)
+
+    val ostream = new FileOutputStream(outFile)
+    val output = new TranscoderOutput(ostream);
+
+    t.transcode(input, output);
+
+    ostream.flush();
+
+    ostream.close();
+
+  }
+}
