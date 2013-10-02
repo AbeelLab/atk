bcb92835a680ad38313e3572e85081dfaaae6491
Thomas Abeel <thomas@abeel.be>
2013-10-02 14:29:02 -0400

    add execution location to output of tools

--- src/atk/util/Tool.scala
+++ src/atk/util/Tool.scala
@@ -64,7 +64,10 @@ trait Tool extends Lines {
   def classFileName() = { Thread.currentThread().getStackTrace()(2).getFileName() };
 
   def classInfo() = { Thread.currentThread().getStackTrace()(2).getClassName() };
+  
+  def executeEnvironment() ={this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()}
+  
 
-  def generatorInfo() = { "# Generated with " + classInfo() + " defined in " + classFileName() + " on " + new Date() + "\n# Please contact Thomas (tabeel@broadinstitute.org) for problems or questions\n#\n# Configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version") }
+  def generatorInfo() = { "# Generated with " + classInfo() + " defined in " + classFileName() + "\n# Executed in "+executeEnvironment+" on " + new Date() + "\n# Please contact Thomas (tabeel@broadinstitute.org) for problems or questions\n#\n# Configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version") }
 
 }
