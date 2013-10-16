e5291dea896701c21e151e8927c1f3b7d36c54be
Thomas Abeel <thomas@abeel.be>
2013-10-16 14:44:39 -0400

    URL tools - download url and open as file

---
+++ src/atk/util/URLTools.scala
@@ -0,0 +1,33 @@
+package atk.util
+
+import java.io.File
+import java.net.URL
+import java.io.FileOutputStream
+import org.apache.commons.io.IOUtils
+/**
+ * Small tools to work with URLs.
+ * 
+ * @author Thomas Abeel
+ */
+object URLTools {
+
+  def asFile(url: String, keep: Boolean = false) = {
+    val id = MD5Tools.md5(url)
+    val file = new File(id + ".urltools.tmp")
+    if (!keep)
+      file.deleteOnExit()
+    if (!file.exists() || file.length() == 0)
+      retrieve(url, file)
+    file;
+  }
+
+  private def retrieve(urlS: String, out: File) = {
+
+    val conn = new URL(urlS).openConnection();
+    val fof = new FileOutputStream(out);
+    IOUtils.copy(conn.getInputStream(), fof);
+    fof.close();
+
+  }
+
+}
