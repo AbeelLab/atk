77ffaaaefeb96ec5206b072d9269d045a77405f0
Thomas Abeel <thomas@abeel.be>
2013-12-05 17:04:22 -0500

    more sensible handling of the log method added init method to set where the log output will go, by default this is standard out

--- src/atk/util/Tool.scala
+++ src/atk/util/Tool.scala
@@ -31,9 +31,8 @@ trait Tool extends Lines {
 
   private var logger: PrintWriter = null;
 
-  private val timestampFormat:SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss-SSS")
+  private val timestampFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss-SSS")
 
-  
   timestampFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
 
   def timestamp(): String = {
@@ -43,14 +42,22 @@ trait Tool extends Lines {
 
   private val startTime = System.currentTimeMillis();
 
+  def init(location: String=null) {
+    if(location==null)
+      logger = new PrintWriter(System.out)
+    else
+    	logger = new PrintWriter(location)
+    logger.print(generatorInfo + "\n")
+  }
+
   def log(str: Any) = {
     if (logger == null) {
-      logger = new PrintWriter(classFileName + "." + System.currentTimeMillis() + ".log")
-      logger.print(generatorInfo + "\n")
+      init()
+      
+
     }
     logger.println(str)
     logger.flush()
-    println(str)
   }
 
   def finish(out: PrintWriter = logger) = {
@@ -64,10 +71,9 @@ trait Tool extends Lines {
   def classFileName() = { Thread.currentThread().getStackTrace()(2).getFileName() };
 
   def classInfo() = { Thread.currentThread().getStackTrace()(2).getClassName() };
-  
-  def executeEnvironment() ={this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()}
-  
 
-  def generatorInfo() = { "# Generated with " + classInfo() + " defined in " + classFileName() + "\n# Executed in "+executeEnvironment+" on " + new Date() + "\n# Please contact Thomas (tabeel@broadinstitute.org) for problems or questions\n#\n# Configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version") }
+  def executeEnvironment() = { this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() }
+
+  def generatorInfo() = { "# Generated with " + classInfo() + " defined in " + classFileName() + "\n# Executed in " + executeEnvironment + " on " + new Date() + "\n# Please contact Thomas (tabeel@broadinstitute.org) for problems or questions\n#\n# Configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version") }
 
 }
