23fe828d82cdb2ab036515859ee99a60bf2ae806
Thomas Abeel <thomas@abeel.be>
2013-12-20 15:49:30 -0500

    +license

--- src/atk/venn/Venn.scala
+++ src/atk/venn/Venn.scala
@@ -1,3 +1,17 @@
+/*
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * or send a letter to Creative Commons, 444 Castro Street,
+ * Suite 900, Mountain View, California, 94041, USA.
+ *
+ * A copy of the license is included in LICENSE.txt
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * Copyright 2005-2013 Thomas Abeel
+ */
 package venn
 
 import scala.io.Source
