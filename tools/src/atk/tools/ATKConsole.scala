ca0832904330fa227eac4552482068f476539f3c
Thomas Abeel <thomas@abeel.be>
2014-02-24 11:02:24 -0500

    basic histogram program

---
+++ tools/src/atk/tools/ATKConsole.scala
@@ -0,0 +1,30 @@
+package atk.tools
+
+object ATKConsole {
+
+ def main(args: Array[String]): Unit = {
+
+    if (args.length == 0) {
+
+      listInstructions
+    } else {
+      args(0) match {
+        case "list" => listInstructions
+        case "help" => listInstructions
+        case "histogram" => Histogram.main(args.drop(1))
+        case _ => listInstructions
+      }
+    }
+
+  }
+
+  def listInstructions() {
+    println("Usage:java -jar atk.jar [instruction] [instruction options...]")
+    println("Instructions:")
+    println("\thistogram\t\tCreate a histogram plot from data in file")
+    println("\tlist\t\tShow help")
+    println("\thelp\t\tShow help")
+
+  }
+
+}
