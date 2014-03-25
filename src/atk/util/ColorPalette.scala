8080c1d6636a59410b000981e948b717b768b92d
Thomas Abeel <thomas@abeel.be>
2014-03-25 16:07:18 -0400

    replaced black and gray with ligher variants

--- src/atk/util/ColorPalette.scala
+++ src/atk/util/ColorPalette.scala
@@ -9,7 +9,7 @@ import java.awt.Color
  */
 object ColorPalette {
 
-  private val colors = List(new Color(230, 159, 0), new Color(86, 180, 233), new Color(0, 158, 115), new Color(240, 228, 66), new Color(0, 114, 178), new Color(213, 94, 0), new Color(204, 121, 167),Color.BLACK,Color.GRAY)
+  private val colors = List(new Color(230, 159, 0), new Color(86, 180, 233), new Color(0, 158, 115), new Color(240, 228, 66), new Color(0, 114, 178), new Color(213, 94, 0), new Color(204, 121, 167),Color.GRAY,Color.LIGHT_GRAY)
 
   def apply(n: Int): Color = {
     colors(n % colors.length)
