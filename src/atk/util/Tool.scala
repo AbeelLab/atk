d4e278e34bb92227060232226e3cb0475b1cb223
Thomas Abeel <thomas@abeel.be>
2014-11-05 12:07:46 +0100

    added support for detailed version information

--- src/atk/util/Tool.scala
+++ src/atk/util/Tool.scala
@@ -35,6 +35,9 @@ object Tool extends Tool{
  */
 trait Tool extends Lines {
 
+  
+  val version="There is no version available for this tool"
+    
   val nfP = NumberFormat.getPercentInstance(Locale.US)
   nfP.setMaximumFractionDigits(2)
 
@@ -86,6 +89,17 @@ trait Tool extends Lines {
 
   def executeEnvironment() = { this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() }
 
-  def generatorInfo() = { "# Generated with " + classInfo() + " defined in " + classFileName() + "\n# Binary in " + executeEnvironment + " on " + new Date() + "\n# Working directory: " + new File(".").getAbsolutePath() + "\n#\n# Please contact Thomas (tabeel@broadinstitute.org) for problems or questions\n#\n# Configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version") }
+  def generatorInfo() = { 
+    "# Generated with " + classInfo() + " defined in " + classFileName() + "\n"+
+    "# Binary in " + executeEnvironment + " on " + new Date() + "\n"+
+    "# Working directory: " + new File(".").getAbsolutePath() + "\n#\n"+
+    "# Please contact Thomas Abeel (tabeel@broadinstitute.org) for problems or questions\n#\n"+
+    "# Configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version")+"\n#\n"+
+    "# Detailed version information: \n" +
+    "#     "+version.split("\n").mkString("\n#     ")
+    
+    
+  
+  }
 
 }
