a73881122d7eb82c1a1c7d648da7fb25e44ce219
Thomas Abeel <thomas@abeel.be>
2014-12-03 13:07:36 +0100

    have fail-safe for time outs

--- src/atk/io/URLCache.scala
+++ src/atk/io/URLCache.scala
@@ -26,22 +26,28 @@ object URLCache {
     if (!cached.exists() || cached.length() == 0 || old(cached.lastModified())) {
 
       //    println(hmac)
-	/* Wait at least 10 seconds between queries */
-      while (System.currentTimeMillis() - lastQuery < 10 * 1000) {
-        Thread.sleep(1000)
-      }
-      lastQuery = System.currentTimeMillis()
 
-      val u = new URL(url);
-      val conn = u.openConnection();
-      val lines = Source.fromInputStream(conn.getInputStream()).getLines.toList
-      val pw = new PrintWriter(cached)
-      pw.println(lines.mkString("\n"))
-      pw.close
-      lines
-    } else {
-      Source.fromFile(cached).getLines.toList
+      cached.delete()
+      while (!cached.exists()) {
+        /* Wait at least 15 seconds between queries */
+        while (System.currentTimeMillis() - lastQuery < 15 * 1000) {
+          Thread.sleep(1000)
+        }
+        lastQuery = System.currentTimeMillis()
+
+        val u = new URL(url);
+        val conn = u.openConnection();
+        val lines = Source.fromInputStream(conn.getInputStream()).getLines.toList
+        
+        if (!lines.contains("Timed out")) {
+          val pw = new PrintWriter(cached)
+          pw.println(lines.mkString("\n"))
+          pw.close
+        }
+
+      }
     }
+    Source.fromFile(cached).getLines.toList
 
   }
 
