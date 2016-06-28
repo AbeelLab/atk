3b4319dee53975cf3bdfc8efed6c8f67a4a5203d
Thomas Abeel <thomas@abeel.be>
2016-06-28 17:21:22 +0200

    more output options

--- src/atk/io/HTMLPrintWriter.scala
+++ src/atk/io/HTMLPrintWriter.scala
@@ -15,45 +15,50 @@
 package atk.io
 
 import java.io.PrintWriter
+import atk.io.HTMLPrintWriter._
+
+object HTMLPrintWriter {
+  def b(str: String) = tag(str, "b")
+
+  def i(str: String) = tag(str, "i")
+  def a(link: String,label:String=null) = "<a href='"+link+"'>"+(if(label==null)link else label)+"</a>"
+
+  def td(str: String) = tag(str, "td")
+  def th(str: String) = tag(str, "th")
+
+  def tr(str: String) = tag(str, "tr")
+  def pre(str: String) = tag(str, "pre")
+
+  def tag(str: String, tag: String) = { "<" + tag + ">" + str + "</" + tag + ">" }
+  def h1(str: String) = tag(str, "h1")
+
+  def h2(str: String) = tag(str, "h2")
+
+  def h3(str: String) = tag(str, "h3")
+
+  def h4(str: String) = tag(str, "h4")
+}
 
 class HTMLPrintWriter(str: String) extends PrintWriter(str: String) {
 
-  def printTableOpen(option:String="") = {
-    println("<table "+option+" >")
+  def printTableOpen(option: String = "") = {
+    println("<table " + option + " >")
   }
 
-  def h1(str:String)=tag(str,"h1")
-  
-  def h2(str:String)=tag(str,"h2")
-  
-  def h3(str:String)=tag(str,"h3")
-  
-  def h4(str:String)=tag(str,"h4")
-  
   def printTableClose = { println("</table>") }
 
-  def pre(str:String)=tag(str,"pre")
-  
-  def tag(str:String,tag:String)={println("<"+tag+">"+str+"</"+tag+">")}
-  
-  
+  def ptag(str: String, tagX: String) = { println(tag(str, tagX)) }
+
   def printTableRow(row: String) = {
     println("<tr><td>" + row.replaceAll("\t", "</td><td>") + "</td></tr>")
   }
-    def printTableHeading(row: String) = {
+  def printTableHeading(row: String) = {
     println("<tr><th>" + row.replaceAll("\t", "</th><th>") + "</th></tr>")
   }
-  
-   @Deprecated
-  def wrapBold(str:String)="<b>"+str+"</b>"
-  
-  def b(str:String)=tag(str,"b")
-  
-  def i(str:String)=tag(str,"i")
-  
-  def td(str:String)=tag(str,"td")
-  
-  def tr(str:String)=tag(str,"tr")
-  
-  
+
+  @Deprecated
+  def wrapBold(str: String) = "<b>" + str + "</b>"
+
+  def ptr(row:String) = { println(tr(row)) }
+
 }
