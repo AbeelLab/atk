17fa994dd0df151c439719fbac6d42cf435f33e0
Thomas Abeel <thomas@abeel.be>
2016-09-12 09:07:22 +0200

    no reason to filter #

--- src/atk/compbio/fastq/FastQFile.scala
+++ src/atk/compbio/fastq/FastQFile.scala
@@ -8,7 +8,7 @@ object FastQFile {
   case class FastqRecord(name: String, seq: String, qual: String);
 
   def apply(path: File): Iterator[FastqRecord] = {
-    Source.fromFile(path).getLines.filterNot(_.startsWith("#")).filterNot(_.trim().length() == 0).grouped(4).map(group => {
+    Source.fromFile(path).getLines.filterNot(_.trim().length() == 0).grouped(4).map(group => {
       new FastqRecord(group(0).drop(1),group(1),group(3))
     })
   }
