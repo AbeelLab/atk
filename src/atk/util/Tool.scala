74a7dc5028e042216c3eb2d11babac3479395c93
Thomas Abeel <thomas@abeel.be>
2016-09-28 11:40:34 -0400

    Added progress method for tracking progress in long running tasks

--- src/atk/util/Tool.scala
+++ src/atk/util/Tool.scala
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions
  * and limitations under the License.
  *
- * Copyright 2005-2013 Thomas Abeel
+ * Copyright 2005-2016 Thomas Abeel
  */
 package atk.util
 
@@ -23,9 +23,11 @@ import java.text.SimpleDateFormat
 import java.util.TimeZone
 import java.text.NumberFormat
 import java.util.Locale
+import java.time.LocalDateTime
+import atk.io.NixWriter
 
-object Tool extends Tool{
-  
+object Tool extends Tool {
+
 }
 
 /**
@@ -35,18 +37,17 @@ object Tool extends Tool{
  */
 trait Tool extends Lines {
 
-  
-  val version="There is no version information available for this tool"
-  
-  val description="There is no description available for this tool"  
-    
+  val version = "There is no version information available for this tool"
+
+  val description = "There is no description available for this tool"
+
   val nfP = NumberFormat.getPercentInstance(Locale.US)
   nfP.setMaximumFractionDigits(2)
 
   val nf = NumberFormat.getInstance(Locale.US)
   nf.setMaximumFractionDigits(2)
-  
-  lazy val naturalOrdering=Ordering.comparatorToOrdering(NaturalOrderComparator.NUMERICAL_ORDER_IGNORE_CASE)
+
+  lazy val naturalOrdering = Ordering.comparatorToOrdering(NaturalOrderComparator.NUMERICAL_ORDER_IGNORE_CASE)
   private var logger: PrintWriter = null;
 
   private val timestampFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss-SSS")
@@ -59,13 +60,24 @@ trait Tool extends Lines {
   }
 
   private val startTime = System.currentTimeMillis();
+  private var progressCounter = 1
 
-  def init(location: String = null,config:AnyRef=null) {
-    if (location == null)
+  def progress(reportFreq: Int) = {
+    if (progressCounter % reportFreq == 0) {
+      val interval = System.currentTimeMillis() - startTime
+      log("Processing: " + progressCounter + "\t" + new TimeInterval(interval) + "\t" + nf.format(progressCounter * 1000L / (interval + .1)) + " units/s\t" + LocalDateTime.now())
+    }
+    progressCounter += 1
+  }
+
+  def init(location: String = null, config: AnyRef = null): Unit = {
+    if (location == null){
       logger = new PrintWriter(System.out)
+      logger.print(generatorInfo(config) + "\n")
+    }
     else
-      logger = new PrintWriter(location)
-    logger.print(generatorInfo(config) + "\n")
+      logger = new NixWriter(location,config)
+    
   }
 
   def log(str: Any) = {
@@ -85,43 +97,40 @@ trait Tool extends Lines {
       out.close()
     }
   }
-  private def classFileName() = {Thread.currentThread().getStackTrace().takeRight(1)(0).getFileName() };
+  private def classFileName() = { Thread.currentThread().getStackTrace().takeRight(1)(0).getFileName() };
 
   private def classInfo() = { Thread.currentThread().getStackTrace().takeRight(1)(0).getClassName() };
 
   private def executeEnvironment() = { this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() }
 
-  private def getDeclaredFields(cc: AnyRef) ={
-    val m=(Map[String, Any]() /: cc.getClass.getDeclaredFields) { (a, f) =>
+  private def getDeclaredFields(cc: AnyRef) = {
+    val m = (Map[String, Any]() /: cc.getClass.getDeclaredFields) { (a, f) =>
       f.setAccessible(true)
       a + (f.getName -> f.get(cc))
     }
     m.toList.sortBy(_._1)
   }
-  
- 
+
   /* Added for legacy purposes */
-  def generatorInfo():String = {generatorInfo(null)};
-  
-  def generatorInfo(config:AnyRef=null):String = { 
-    "# Generated with    " + classInfo()  + "\n"+
-    "# Source code in    " + classFileName() + "\n"+
-    "# Binary in         " + executeEnvironment + "\n"+
-    "# Date              " + new Date() + "\n"+
-    "# Working directory " + new File(".").getAbsolutePath() + "\n#\n"+
-    "# Please contact Thomas Abeel for problems or questions:\n#\tt.abeel@tudelft.nl\n#\n"+
-    "# Machine configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version")+"\n#\n"+
-    "# Description: \n" +
-    "#     "+description.split("\n").mkString("\n#     ")+
-    "#\n"+
-    "# Detailed version information: \n" +
-    "#     "+version.split("\n").mkString("\n#     ")+
-    (if(config!=null){
-      "\n# Tool configuration: \n#     " + getDeclaredFields(config).mkString("\n#     ")
-    }else "")
-    
-    
-  
+  def generatorInfo(): String = { generatorInfo(null) };
+
+  def generatorInfo(config: AnyRef = null): String = {
+    "# Generated with    " + classInfo() + "\n" +
+      "# Source code in    " + classFileName() + "\n" +
+      "# Binary in         " + executeEnvironment + "\n" +
+      "# Date              " + new Date() + "\n" +
+      "# Working directory " + new File(".").getAbsolutePath() + "\n#\n" +
+      "# Please contact Thomas Abeel for problems or questions:\n#\tt.abeel@tudelft.nl\n#\n" +
+      "# Machine configuration summary: \n#\t " + "Current date and time: " + new Date() + "\n#\t " + "Number of processors: " + Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n#\t " + "Free memory :" + Runtime.getRuntime().freeMemory() + "\n#\t " + "Max memory: " + Runtime.getRuntime().maxMemory() + "\n#\t " + "Total JVM: " + Runtime.getRuntime().totalMemory() + "\n#\t " + "OS: " + ManagementFactory.getOperatingSystemMXBean().getName() + " " + ManagementFactory.getOperatingSystemMXBean().getVersion() + "\n#\t " + "Architecture: " + ManagementFactory.getOperatingSystemMXBean().getArch() + "\n#\t " + "JVM version: " + System.getProperty("java.version") + "\n#\n" +
+      "# Description: \n" +
+      "#     " + description.split("\n").mkString("\n#     ") +
+      "\n#\n" +
+      "# Detailed version information: \n" +
+      "#     " + version.split("\n").mkString("\n#     ") +
+      (if (config != null) {
+        "\n# Tool configuration: \n#     " + getDeclaredFields(config).mkString("\n#     ")
+      } else "")
+
   }
 
 }
