7a59295f1f036c0e192d92bd3b78a3c5a98ff907
Thomas Abeel <thomas@abeel.be>
2016-09-28 11:40:07 -0400

    gave nixwriter some more bells and whistles

--- src/atk/io/NixWriter.scala
+++ src/atk/io/NixWriter.scala
@@ -2,16 +2,27 @@ package atk.io
 
 import java.io.PrintWriter
 import java.io.File
+import java.time.LocalDateTime
+import atk.util.Tool
 
-class NixWriter(f: String) extends PrintWriter(f) {
+class NixWriter(f: String,config:AnyRef) extends PrintWriter(f) with Tool{
 
-  def this(f:File)={
-    this(f.toString)
+  def this(f:File,config:AnyRef)={
+    this(f.toString,config)
   }
   
+  
+  println(generatorInfo(config))
   
   override def println(str: String) {
     print(str + "\n")
   }
 
+  override def close(){
+    println("## This analysis finished " + LocalDateTime.now())
+    super.close
+    
+  }
+  
+  
 }
