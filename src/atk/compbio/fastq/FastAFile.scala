d7ccce62960a0cc5b7f5f5aa4d0e589312925a33
Thomas Abeel <thomas@abeel.be>
2016-10-05 08:12:14 +0200

    fixed for multi-line fasta files

--- src/atk/compbio/fastq/FastAFile.scala
+++ src/atk/compbio/fastq/FastAFile.scala
@@ -8,9 +8,19 @@ object FastAFile {
   case class FastaRecord(name: String, seq: String);
 
   def apply(path: File): Iterator[FastaRecord] = {
-    Source.fromFile(path).getLines.filterNot(_.trim().length() == 0).grouped(2).map(group => {
-      new FastaRecord(group(0).drop(1),group(1))
-    })
+    val lines=Source.fromFile(path).getLines.filterNot(_.trim().length() == 0).toList
+    val idx=lines.zipWithIndex.filter(_._1(0)=='>').map(_._2)
+ 
+    val slices=idx.sliding(2).toList
+    
+    val list=for(slice <- slices.iterator) yield{
+      val (from,to)=slice(0)->slice(1)
+      val group=lines.slice(from, to).toList
+      new FastaRecord(group(0).drop(1),group.drop(1).mkString(""))
+    }
+    list
+    
+    
   }
 
   def apply(path: String): Iterator[FastaRecord] = {
