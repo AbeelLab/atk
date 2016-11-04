2e075b66ef09743a0a43d292ced51e68062a1ced
Thomas Abeel <thomas@abeel.be>
2016-11-04 10:42:24 +0100

    added option to skip printing header

--- src/atk/io/NixWriter.scala
+++ src/atk/io/NixWriter.scala
@@ -5,14 +5,14 @@ import java.io.File
 import java.time.LocalDateTime
 import atk.util.Tool
 
-class NixWriter(f: String,config:AnyRef=null) extends PrintWriter(f) with Tool{
+class NixWriter(f: String,config:AnyRef=null,noheader:Boolean=false) extends PrintWriter(f) with Tool{
 
   def this(f:File,config:AnyRef)={
     this(f.toString,config)
   }
   
-  
-  println(generatorInfo(config))
+  if(!noheader)
+    println(generatorInfo(config))
   
   override def println(str: String) {
     print(str + "\n")
