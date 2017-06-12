f851d7b62c64695f3493b83c7a4d4e9e63de5342
Thomas Abeel <thomas@abeel.be>
2017-06-12 13:25:11 +0200

    added block timing code

--- src/atk/util/Tool.scala
+++ src/atk/util/Tool.scala
@@ -132,5 +132,13 @@ trait Tool extends Lines with LoggingTrait{
       } else "")
 
   }
+ /* Elapsed time function */
+    def time[R](block: => R): R = {
+      val t0 = System.currentTimeMillis()
+      val result = block // call-by-name
+      val t1 = System.currentTimeMillis()
+      println("Elapsed time: " + (t1 - t0) + "ms")
+      result
+    }
 
 }
