aaf401c77461b69c1e6bd2af69e4b01072044e2c
Thomas Abeel <thomas@abeel.be>
2020-06-13 23:06:04 +0200

    multidownload appears to be working

--- test/src/atk/io/TestURLCache.scala
+++ test/src/atk/io/TestURLCache.scala
@@ -15,6 +15,12 @@ class TestURLCache extends FunSuite  {
     println(URLCache.query("https://www.brickwatch.net/de-DE/",bypassCertificates=true))
 
   }
+  
+  test("testMultiDownload"){
+    val us = URLCache.query("https://brickset.com/buy/amazon", URLCache.DAY, cookies = "PreferredCountry2=CountryCode=US&CountryName=United States", key = "brickset_US")
+    val eu = URLCache.query("https://brickset.com/buy/amazon", URLCache.DAY, encoding = "UTF-8", cookies = "PreferredCountry2=CountryCode=NL&CountryName=Netherlands", key = "brickset_EU")
+
+  }
 
   
 }
