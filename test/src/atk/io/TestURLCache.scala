40c400df16cd48fee06737be9483f6ff5977d713
Thomas Abeel <thomas@abeel.be>
2020-06-13 23:16:31 +0200

    some testing for URLCache

--- test/src/atk/io/TestURLCache.scala
+++ test/src/atk/io/TestURLCache.scala
@@ -5,6 +5,10 @@ import java.io.PrintWriter
 
 object TestURLCache {
   def main(args: Array[String]) {
+    URLCache.debug=true
+    URLCache.queryWait = 1000
+   val us = URLCache.query("https://brickset.com/buy/amazon", URLCache.DAY, cookies = "PreferredCountry2=CountryCode=US&CountryName=United States", key = "brickset_US")
+    val eu = URLCache.query("https://brickset.com/buy/amazon", URLCache.DAY, encoding = "UTF-8", cookies = "PreferredCountry2=CountryCode=NL&CountryName=Netherlands", key = "brickset_EU")
 
   }
 }
